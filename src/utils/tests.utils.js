57bc7be50641a52fab5a319cdaada93425edf3c9
phellipe <phellipe.andrade@hotmail.com>
2018-02-18 17:09:09 -0300

    initial setup

---
+++ src/utils/tests.utils.js
@@ -0,0 +1,18 @@
+export const defaultTimes = 200;
+
+export const genLoop = number => Array(number || defaultTimes).fill();
+
+export const defaultLoop = genLoop();
+
+
+export const getTimesGenerated = map => map.reduce((obj, b) => {
+  obj[b.id] = ++obj[b.id] || 1;
+  return obj;
+}, {});
+
+export const between = (x, min, max) => x >= min && x <= max;
+
+export const diff = (a, b) => Math.abs(a - b);
+
+export const calculatePercent = (portion, total, precision = 2) => 
+  Number(((portion / total) * 100).toFixed(precision));
